ba9d49904a3207f968d62725e301af06bff098aa
parkJi-yun <nmjk0123@naver.com>
2023-05-26 22:26:11 +0900

    movie update

---
+++ src/store/about.js
@@ -0,0 +1,10 @@
+import { Store } from '../core/heropy'
+
+export default new Store({
+  photo: 'https://parkji-yun.github.io/assets/images/avthm.jpg',
+  name: 'Park_JiYun',
+  email: 'nmjk0123@gmail.com',
+  blog: 'https://parkji-yun.github.io/',
+  github: 'https://github.com/parkJi-yun',
+  repository: 'https://github.com/parkJi-yun/Movie_Search'
+})
